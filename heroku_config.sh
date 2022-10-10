1b6d71b3aecdedb4e4c900840d6653b238343fa9
Sebastian Lopez Wells <106717053+SebastianLopezWells@users.noreply.github.com>
2022-10-10 01:04:26 -0700

    Code

--- heroku_config.sh
+++ heroku_config.sh
@@ -1,6 +1,6 @@
 heroku config:set SECRET_KEY=whatever
 
-heroku config:set EMAIL_BACKEND=smtp
+heroku config:set EMAIL_BACKEND=console
 heroku config:set EMAIL_HOST=smtp.sendgrid.net
 heroku config:set EMAIL_HOST_USER=apikey
 heroku config:set EMAIL_HOST_PASSWORD=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
